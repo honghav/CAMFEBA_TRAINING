30a1de5e73656b7dff2e8316d98204068701eae7
honghav <158126590+honghav@users.noreply.github.com>
2025-09-16 16:45:57 +0700

    Fixing1

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -15,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Tailwind + App Styles/JS -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans text-gray-900 antialiased bg-gray-100 dark:bg-gray-900">
 
